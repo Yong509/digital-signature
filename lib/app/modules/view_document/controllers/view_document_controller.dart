19fbb86a2d4db39132f4126dc8fbdaf174d46c70
yongggnd <yongkerkwith@gmail.com>
2022-11-27 00:32:22 +0700

    regenerate cert pfx, init sign doc and save doc, share file by share plus

--- lib/app/modules/view_document/controllers/view_document_controller.dart
+++ lib/app/modules/view_document/controllers/view_document_controller.dart
@@ -1,7 +1,21 @@
+import 'dart:io';
+
+import 'package:digital_signature/app/data/models/document_model.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
+import 'dart:ui' as ui;
+import 'package:syncfusion_flutter_pdf/pdf.dart';
+import 'package:syncfusion_flutter_signaturepad/signaturepad.dart';
 
 class ViewDocumentController extends GetxController {
   final showSignaturePad = false.obs;
+
+  List<int>? signedDocumentByte;
+  Uint8List? signedDocument;
 
   @override
   void onInit() {
@@ -16,5 +30,66 @@ class ViewDocumentController extends GetxController {
   @override
   void onClose() {
     super.onClose();
+  }
+
+  signAndSaveDocuments(
+      {required GlobalKey<SfSignaturePadState> signaturePadGlobalKey,
+      required DocumentModel sourceDocument}) async {
+    ByteData certBytes =
+        await rootBundle.load('assets/certificate/certificate.pfx');
+    final Uint8List certificateBytes = certBytes.buffer.asUint8List();
+
+    final ByteData docBytes = await rootBundle.load(sourceDocument.file);
+    final Uint8List documentBytes = docBytes.buffer.asUint8List();
+
+    PdfDocument document = PdfDocument(inputBytes: documentBytes);
+
+    int pageCount = document.pages.count;
+    PdfPage page = document.pages[pageCount - 1];
+
+    final image =
+        await signaturePadGlobalKey.currentState!.toImage(pixelRatio: 3.0);
+    final bytes = await image.toByteData(format: ui.ImageByteFormat.png);
+
+    PdfSignatureField signatureField = PdfSignatureField(
+      page,
+      'signature',
+      bounds: const Rect.fromLTRB(300, 500, 550, 700),
+      signature: PdfSignature(
+        certificate: PdfCertificate(certificateBytes, 'password'),
+        contactInfo: 'yong@fmail.com',
+        reason: 'Approved document',
+        digestAlgorithm: DigestAlgorithm.sha256,
+        cryptographicStandard: CryptographicStandard.cms,
+      ),
+    );
+
+    PdfGraphics? graphics = signatureField.appearance.normal.graphics;
+
+    graphics?.drawImage(PdfBitmap(bytes!.buffer.asUint8List()),
+        const Rect.fromLTWH(0, 0, 250, 200));
+
+    document.form.fields.add(signatureField);
+    document.form.flattenAllFields();
+
+    signedDocumentByte = await document.save();
+
+    final directory = await getApplicationSupportDirectory();
+    final path = directory.path;
+    File file = File('$path/${sourceDocument.id}_output.pdf');
+    await file.writeAsBytes(signedDocumentByte!, flush: true);
+
+    signedDocument = Uint8List.fromList(signedDocumentByte!);
+    document.dispose();
+
+    showSignaturePad.value = false;
+    update();
+  }
+
+  shareDocument({required DocumentModel sourceDocument}) async {
+    final directory = await getApplicationSupportDirectory();
+    final path = directory.path;
+
+    Share.shareXFiles([XFile('$path/${sourceDocument.id}_output.pdf')]);
   }
 }
--- lib/app/modules/view_document/views/view_document_view.dart
+++ lib/app/modules/view_document/views/view_document_view.dart
@@ -12,6 +12,7 @@ class ViewDocumentView extends GetView<ViewDocumentController> {
   ViewDocumentView({Key? key}) : super(key: key);
 
   final DocumentModel doc = Get.arguments;
+  final GlobalKey<SfSignaturePadState> signaturePadGlobalKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -23,23 +24,58 @@ class ViewDocumentView extends GetView<ViewDocumentController> {
             title: const Text('ViewDocumentView'),
             centerTitle: true,
             actions: [
-              IconButton(
-                onPressed: () {
-                  controller.showSignaturePad.value =
-                      !controller.showSignaturePad.value;
-                  controller.update();
-                },
-                icon: const Icon(Icons.gesture),
-              ),
+              (controller.signedDocument == null)
+                  ? IconButton(
+                      onPressed: () {
+                        controller.showSignaturePad.value =
+                            !controller.showSignaturePad.value;
+                        controller.update();
+                      },
+                      icon: const Icon(Icons.gesture),
+                    )
+                  : IconButton(
+                      onPressed: () {
+                        controller.shareDocument(sourceDocument: doc);
+                      },
+                      icon: const Icon(Icons.share),
+                    ),
             ],
           ),
           body: Column(
             children: [
               Expanded(
-                child: SfPdfViewer.asset(doc.file),
+                child: (controller.signedDocument == null)
+                    ? SfPdfViewer.asset(doc.file)
+                    : SfPdfViewer.memory(controller.signedDocument!),
               ),
               (controller.showSignaturePad.value == true)
-                  ? SfSignaturePad()
+                  ? Column(
+                      children: [
+                        SfSignaturePad(
+                          key: signaturePadGlobalKey,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () {
+                                controller.signAndSaveDocuments(
+                                    signaturePadGlobalKey:
+                                        signaturePadGlobalKey,
+                                    sourceDocument: doc);
+                              },
+                              child: const Text("Sign document"),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                signaturePadGlobalKey.currentState!.clear();
+                              },
+                              child: const Text("Clear"),
+                            ),
+                          ],
+                        )
+                      ],
+                    )
                   : Container()
             ],
           ),
